a47ea53ec9001555b3ed45dc15acf0febda8b3cf
Gustavo <glunnye2@gmail.com>
2025-10-08 11:58:56 -0300

    NOVA HOMEPAGE E MELHORIAS

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("inicio/", views.inicio, name="inicio"),
+    path("cadastro_questao/", views.cadastro_questao, name="cadastro_questao"),
     path('buscar-conteudos/', views.buscar_conteudos_filho, name="buscar_conteudos")
 ]
--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from .forms import QuestaoForm
 from django.http import JsonResponse
 from django.contrib import messages
 
-def index(request):
+def cadastro_questao(request):
     if request.method == 'POST':
         data = request.POST.copy()
         for campo in ['unidade', 'topico', 'subtopico', 'categoria']:
@@ -15,14 +15,14 @@ def index(request):
         if form.is_valid():
             form.save()
             messages.success(request, "Questão cadastrada com sucesso!")
-            return redirect('index')
+            return redirect('cadastro_questao')
         else:
             messages.error(request, "Erro ao cadastrar questão.")
     else:   
         form = QuestaoForm()
         
     areas = Conteudo.objects.filter(tipo='area')
-    return render(request, 'app/index.html', {'form':form, 'areas': areas})
+    return render(request, 'app/cadastro_questao.html', {'form':form, 'areas': areas})
 
 def buscar_conteudos_filho(request):
     pai_id = request.GET.get('pai_id')
